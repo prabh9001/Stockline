1ce5fd715e46adb0d36a7d5dfe3a7be424fd9be4
Prabhjeet Singh <prabhjeetsingh6360@gmail.com>
2024-04-22 19:38:40 +0530

    About section

--- pages/02_📩Aboutme.py
+++ pages/02_📩Aboutme.py
@@ -6,5 +6,20 @@ st.set_page_config(
 )
 
 st.markdown(
-                                                     """# 👤 **About ME**
-)
+    """# 👤 **About ME**
+### 💺 **Project Supervisor**
+ 
+Prabhjeet Singh. I am passionate about using data and machine learning to solve real-world problems and help people make better decisions. In their free time, they enjoy learning new technologies, reading about the latest developments in machine learning, and exploring the outdoors.
+
+Project Title : Stockline [Which Predicts the Stock Price after Analysing the previous data]
+
+Project Owened : Prabhjeet Singh
+
+Studied : BCA in 2024 in Guru Nanak Dev University
+
+City : Amritsar,Punjab
+
+### ☎️ **ContactMe**
+If You are facing any problem or find bug or have any suggestion, feel free to contact me on :
+## 📩 : prabh148254@gmail.com
+""")
