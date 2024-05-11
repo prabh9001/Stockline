7e1a9b72e8ddc7de6bb7ea8a55c9ec6ed4f0056d
Prabhjeet Singh <prabhjeetsingh6360@gmail.com>
2024-05-11 17:34:30 +0530

    adding def

--- pages/01_🤞Project.py
+++ pages/01_🤞Project.py
@@ -164,8 +164,9 @@ with  Two_Hundred_Days_EMA_Chart:
     st.pyplot(fig)
 
 
-
-st.title('Linear Regression - Actual vs Predicted Values')
+def LinearRegression():
+    
+    st.title('Linear Regression - Actual vs Predicted Values')
 from sklearn.linear_model import LinearRegression
 # Example data
 X_train = ny.random.rand(2274, 1)  # Replace with your actual features
